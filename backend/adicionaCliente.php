7d7c4c0d1199e940fc714d62eabbdd889195ba57
IsaM <bellam2513@gmail.com>
2024-08-28 14:15:52 -0300

    Basico backend, deletando pasta tela-inicial

---
+++ backend/adicionaCliente.php
@@ -0,0 +1,26 @@
+<?php
+   if (isset($_POST['nome'])) {
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'projeto-final';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName );
+    $nome = $_POST['nome'];
+    $nome = $_POST['telefone'];
+    $sql = "insert into clientes (nome, telefone) values ('" . $nome . "," . $telefone . "');";
+
+    if (!$conexao) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    if ($conexao->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conexao->close();
+}
+
+?>
