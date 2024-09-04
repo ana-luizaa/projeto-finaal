9f4dbcaadae7a8bc395747a2730c287070c6d907
ana-luizaa <analuiza222r@gmail.com>
2024-09-04 20:58:37 -0300

    back atualizado

--- backend/config.php
+++ backend/config.php
@@ -7,4 +7,12 @@
 
     $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName );
 
+
+    if($conexao->connect_error){
+        echo "Erro";
+    }
+    else{
+        echo "ok";
+        
+    }
 ?>
