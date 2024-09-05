fe1437387bb6ea9927c24dfb6c3ebbe66e9ef35e
ana-luizaa <analuiza222r@gmail.com>
2024-09-04 22:44:33 -0300

    tirando o ok do back

--- backend/config.php
+++ backend/config.php
@@ -8,11 +8,5 @@
     $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName );
 
 
-    if($conexao->connect_error){
-        echo "Erro";
-    }
-    else{
-        echo "ok";
-        
-    }
+   
 ?>
